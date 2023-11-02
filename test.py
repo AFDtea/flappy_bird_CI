2b00090debde60929fa19a034fb48f02d8d86aad
AFDtea <77896483+AFDtea@users.noreply.github.com>
2023-11-02 09:54:09 -0400

    Fixed the Practice

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
 import gym
 
-env = gym.make("Mountain-Car-v0")
+env = gym.make("CartPole-v1", render_mode="human")
 env.reset()
 
-done = False
+terminated = False
 
-while not done:
-    action = 2
-    new_state, reward, done, = env.step(action)
-    env.render()
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
 
 env.close()
+
+# import gym
+# env = gym.make("LunarLander-v2", render_mode="human")
+# observation, info = env.reset(seed=42)
+# for _ in range(1000):
+#    action = env.action_space.sample()  # this is where you would insert your policy
+#    observation, reward, terminated, truncated, info = env.step(action)
+
+#    if terminated or truncated:
+#       observation, info = env.reset()
+
+# env.close()
