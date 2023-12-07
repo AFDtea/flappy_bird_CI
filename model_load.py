cc32b00acec2b2251c3f252726a14d6518a324e2
AFDtea <77896483+AFDtea@users.noreply.github.com>
2023-12-07 18:52:47 -0500

    Cleaned up Extra files and Added comments
    Removed all of the old training logs and testing files as well as added comments to the remaining python files and changed their names to match their purpose

--- model_load.py
+++ model_load.py
@@ -1,18 +1,19 @@
 import gymnasium as gym
 import flappy_bird_gymnasium
 from stable_baselines3 import PPO
-import os
-import numpy as np
 
+# loading the FlappyBird environment
 env = gym.make("FlappyBird-v0", render_mode="human")
 env.reset()
 
+# loading the model's weights with the best performance
 models_dir = "models/palm_PPO_2"
 model_path = (f"{models_dir}/7990000.zip")
 
 model = PPO.load(model_path, env = env)
-episodes = 10
 
+# displaying the model running 10 times
+episodes = 10
 for ep in range(episodes):
     obs, info = env.reset()
     terminated = False
